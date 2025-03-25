9bcccc22981e8745a38b0d2594e8a9259bdbfb05
bomikim <kbm8527@gmail.com>
2025-03-25 15:05:20 +0900

    order entity 추가

--- src/main/java/com/starbucks/starvive/order/domain/Order.java
+++ src/main/java/com/starbucks/starvive/order/domain/Order.java
@@ -1,4 +1,16 @@
 package com.starbucks.starvive.order.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
+@Entity
 public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID orderId;
 }
