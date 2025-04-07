777144ffd0ee42e2ff3caf1ec4dbf8bcbb0fa104
jijihorang <jiho7213@gmail.com>
2025-04-07 09:46:11 +0900

    🏁카테고리 CRUD 완료

--- src/main/java/com/starbucks/starvive/category/dto/in/MiddleCategoryRequest.java
+++ src/main/java/com/starbucks/starvive/category/dto/in/MiddleCategoryRequest.java
@@ -1,6 +1,7 @@
 package com.starbucks.starvive.category.dto.in;
 
 import com.starbucks.starvive.category.domain.MiddleCategory;
+import com.starbucks.starvive.category.vo.MiddleCategoryVo;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -18,6 +19,13 @@ public class MiddleCategoryRequest {
         return MiddleCategory.builder()
                 .name(name)
                 .topCategoryId(topCategoryId)
+                .build();
+    }
+
+    public static MiddleCategoryRequest fromEntity(MiddleCategoryVo middleCategoryVo) {
+        return MiddleCategoryRequest.builder()
+                .name(middleCategoryVo.getName())
+                .topCategoryId(middleCategoryVo.getTopCategoryId())
                 .build();
     }
 }
