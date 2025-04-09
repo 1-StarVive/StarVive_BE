1f08d3ce5083b8e31a0a5e35203b721a0f5d14d7
Shin <ysysysy3333@naver.com>
2025-04-09 17:07:15 +0900

    젠킨스 빌드 오류 테스트

--- src/main/java/com/starbucks/starvive/common/config/S3Config.java
+++ src/main/java/com/starbucks/starvive/common/config/S3Config.java
@@ -8,27 +8,33 @@ import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.StringUtils;
 
 @Configuration
 public class S3Config {
 
-    @Value("${spring.cloud.aws.credentials.access-key}")
+    @Value("${cloud.aws.credentials.access-key:}")
     private String accessKey;
 
-    @Value("${spring.cloud.aws.credentials.secret-key}")
+    @Value("${cloud.aws.credentials.secret-key:}")
     private String secretKey;
 
-    @Value("${spring.cloud.aws.region.static}")
+    @Value("${cloud.aws.region.static:ap-northeast-2}")
     private String region;
 
     @Bean
     public AmazonS3 amazonS3() {
-        AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+        AmazonS3ClientBuilder builder = AmazonS3ClientBuilder.standard();
 
-        return AmazonS3ClientBuilder
-                .standard()
-                .withCredentials(new AWSStaticCredentialsProvider(credentials))
-                .withRegion(region)
-                .build();
+        if (StringUtils.hasText(accessKey) && StringUtils.hasText(secretKey)) {
+            AWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+            builder.withCredentials(new AWSStaticCredentialsProvider(credentials));
+        }
+
+        if (StringUtils.hasText(region)) {
+            builder.withRegion(region);
+        }
+
+        return builder.build();
     }
 }
