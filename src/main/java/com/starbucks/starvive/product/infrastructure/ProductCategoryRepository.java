8c8405f04098f1397f8b1b592c3640afdfaa9f9d
jijihorang <jiho7213@gmail.com>
2025-04-15 12:14:12 +0900

    productRequiredInfo Entity 추가

--- src/main/java/com/starbucks/starvive/product/infrastructure/ProductCategoryRepository.java
+++ src/main/java/com/starbucks/starvive/product/infrastructure/ProductCategoryRepository.java
@@ -2,8 +2,10 @@ package com.starbucks.starvive.product.infrastructure;
 
 import com.starbucks.starvive.product.domain.ProductCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
+@Repository
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, UUID> {
 }
