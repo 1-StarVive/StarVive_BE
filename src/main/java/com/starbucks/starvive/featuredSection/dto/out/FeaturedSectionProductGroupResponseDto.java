22b932ad8cdc6ae0d9013f9b495bfbe27762c47f
bomikim <kbm8527@gmail.com>
2025-04-17 14:50:55 +0900

    post, getmapping 수정완료

--- src/main/java/com/starbucks/starvive/featuredSection/dto/out/FeaturedSectionProductGroupResponseDto.java
+++ src/main/java/com/starbucks/starvive/featuredSection/dto/out/FeaturedSectionProductGroupResponseDto.java
@@ -1,14 +1,22 @@
 package com.starbucks.starvive.featuredSection.dto.out;
 
-import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import java.util.List;
 import java.util.UUID;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class FeaturedSectionProductGroupResponseDto {
 
     private UUID featuredSectionsId;
     private List<FeaturedSectionProductResponseDto> products;
+
+    @Builder
+    public FeaturedSectionProductGroupResponseDto(UUID featuredSectionsId,
+                                                  List<FeaturedSectionProductResponseDto> products) {
+        this.featuredSectionsId = featuredSectionsId;
+        this.products = products;
+    }
 }
--- src/main/java/com/starbucks/starvive/featuredSection/dto/out/FeaturedSectionProductResponseDto.java
+++ src/main/java/com/starbucks/starvive/featuredSection/dto/out/FeaturedSectionProductResponseDto.java
@@ -1,24 +1,53 @@
 package com.starbucks.starvive.featuredSection.dto.out;
 
-import jakarta.persistence.Column;
-import lombok.AllArgsConstructor;
+import com.starbucks.starvive.featuredSection.domain.FeaturedSectionProduct;
+import com.starbucks.starvive.image.domain.ProductImage;
+import com.starbucks.starvive.product.domain.Product;
+import com.starbucks.starvive.product.domain.ProductOption;
+import lombok.Builder;
 import lombok.Getter;
-
+import lombok.NoArgsConstructor;
 import java.util.UUID;
 
 @Getter
-@AllArgsConstructor
+@NoArgsConstructor
 public class FeaturedSectionProductResponseDto {
-
 
     private UUID productId;
     private String imageThumbUrl;
     private String imageThumbAlt;
     private String name;
     private int price;
-  //  private int discountRate;
-   // private int discountedPrice;
-  //  @Column(nullable = false)
     private int baseDiscountRate;
 
+    @Builder
+    public FeaturedSectionProductResponseDto(UUID productId,
+                                             String imageThumbUrl,
+                                             String imageThumbAlt,
+                                             String name,
+                                             int price,
+                                             int baseDiscountRate) {
+        this.productId = productId;
+        this.imageThumbUrl = imageThumbUrl;
+        this.imageThumbAlt = imageThumbAlt;
+        this.name = name;
+        this.price = price;
+        this.baseDiscountRate = baseDiscountRate;
+    }
+
+    public static FeaturedSectionProductResponseDto from(
+            FeaturedSectionProduct featuredSectionProduct,
+            Product product,
+            ProductOption option,
+            ProductImage image
+    ) {
+        return FeaturedSectionProductResponseDto.builder()
+                .productId(product.getProductId())
+                .name(product.getName())
+                .price(option.getPrice())
+                .baseDiscountRate(option.getBaseDiscountRate())
+                .imageThumbUrl(image.getImageThumbUrl())
+                .imageThumbAlt(image.getImageThumbAlt())
+                .build();
+    }
 }
