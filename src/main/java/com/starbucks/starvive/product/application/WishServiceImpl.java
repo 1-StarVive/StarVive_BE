4ce1bf441be9b00be687d15529edadb8cfab0fa4
Shin <ysysysy3333@naver.com>
2025-04-17 14:21:06 +0900

    WebHookTest2

--- src/main/java/com/starbucks/starvive/product/application/WishServiceImpl.java
+++ src/main/java/com/starbucks/starvive/product/application/WishServiceImpl.java
@@ -25,7 +25,7 @@ import static com.starbucks.starvive.common.domain.BaseResponseStatus.*;
 @Service
 @RequiredArgsConstructor
 public class WishServiceImpl implements WishService {
-//ㅋ
+//ㅋㅋ
     private final WishRepository wishRepository;
     private final ProductOptionRepository productOptionRepository;
     private final ProductRepository productRepository;
